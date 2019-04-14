986eaa6be474bd1ac0988587c81ba1fe6ff980d5
Flavio Costa <flavioribeiro.costa@gmail.com>
2019-04-15 08:22:19 +1200

    Project 12b: UserDefaults, Codable

---
+++ Project12b/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Flavio Costa on 02/04/19.
+//  Copyright © 2019 cappsule. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, Codable {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
